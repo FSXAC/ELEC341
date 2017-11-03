0a26a9ecca5d4ba07e7b827e2f1f6e38069a7970
o2liu <leoliu_0.37@hotmail.com>
2017-11-02 22:05:27 -0700

    Add files via upload

---
+++ myattemptOfsystem.m
@@ -0,0 +1,167 @@
+% This script sets the model parameters for the SLS 3-D Printer
+
+% Example: Specifying a Dynamics Block
+% n = [1 2 3];
+% d = [4 5 6];
+% Transfer Function = (s^2 + 2s + 3) / (4s^2 + 5s + 6)
+
+% ========================
+% PHYSICAL UNIT CONVERSION
+% ========================
+% Example: if you decide to work in (Kg), all masses must be represented
+%          in (Kg) but the spec sheet may provide masses in (g)
+% Amplifier circuit elements (units normalized)
+R1_ = R1 * 1e6; %see circuit values in the default.m file
+R2_ = R2;
+C_  = C * 1e-6;
+L_  = L * 1e-3;
+
+%properties of solid cylinder
+radius= 100e-3 
+height= 15e-3
+increment= 5e-3
+
+% Prefix conversion
+MILLIS_TO = 1e3;
+TO_MILLIS = 1e-3;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Over-write the default values from DEFAULT.m %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% ==========================
+% Choose Motors
+% ==========================
+
+% Motor Unit Conversions
+% ----------------------
+
+
+% ==========================
+% Motor Parameters
+% ==========================
+
+% Maximum Current
+% ---------------
+
+
+% =============================
+% Q0 : Rotation about y-axis
+% =============================
+
+
+% =====================[Amplifier Dynamics]========================
+% Transfer function coefficients
+Amp0n0 = (C_ * R2_ * R1_) - L_;
+Amp0d0 = C_ * R1_ * R2_;
+Amp0d1 = L_ * C_ * R1_;
+
+% Transfer Function Recomputation
+Amp0n   = Amp0n0;
+Amp0d   = [Amp0d1 Amp0d0];
+AmpSat0 = Big;
+
+% =====================[Electrical Motor Dynamics]========================
+% This specifies the transfer function for the electric motor
+% INPUT: voltage (V)
+% OUTPUT: current (A)
+Elec0n = 1;
+
+Elec0d0 = MotorParam(TermR);
+Elec0d1 = MotorParam(TermL) * MILLIS_TO;
+Elec0d = [Elec0d0, Elec0d1];
+
+% =====================[Torque Const & Back EMF]========================
+% TORQUE CONSTANT
+% - Between the output of electric dynamics and input of mechanical dynamics
+% - Torque = K_T * Current
+% - (Nm)   = K_T * (A) 
+%
+% - K_T has units (Nm/A)
+% - `MotorParam(TorqueK)` has units (mNm/A)
+% - need to convert to (Nm/A)
+TConst0 = MotorParam(TorqueK) * MILLIS_TO;
+
+% SPEED CONSTANT
+% - Feedback gain
+% - Between the output speed (rad/s) to electric dynamic input (V)
+% - Back EMF = K_V * Speed
+% - (V)      = K_V * (rad/s)
+%
+% - K_V has units (V*s/rad)
+% - `MotorParam(SpdK)` has units (rpm/V)
+
+% First we convert from (rpm/V) to ((rad/s)/V)
+BackEMF0_inv = MotorParam(SpdK) * RadPSecPerRPM;
+
+% Then convert from ((rad/s)/V) to (V/(rad/s))
+BackEMF0 = 1 / BackEMF0_inv;
+
+% =====================[Mechanical Motor Dynamics]========================
+% TODO:
+Mech0n  = [1];
+Mech0d  = [1];
+JntSat0 = Big;
+
+
+% =====================[Sensor Dynamics]========================
+% TODO:
+Sens0    = 0;
+SensSat0 = Big;
+
+
+% =====================[Static Friction]========================
+% TODO:
+StFric0 = 0;
+
+
+
+
+% =============================
+% Q1 : Rotation about x-axis
+% =============================
+
+% =====================[Amplifier Dynamics]========================
+% Since the same amplifier is used on both motors, the transfer function is the same
+Amp1n   = Amp0n;
+Amp1d   = Amp0d;
+AmpSat1 = Big;
+
+% =====================[Electrical Motor Dynamics]========================
+Elec1n = Elec0n;
+Elec1d = Elec0d;
+
+% =====================[Torque Const & Back EMF]========================
+% NOTE: Current using identical values from q0 motor
+TConst1  = TConst0;
+BackEMF1 = BackEMF0;
+
+
+% =====================[Mechanical Motor Dynamics]========================
+% TODO:
+Mech1n  = [1];
+Mech1d  = [1];
+JntSat1 = Big;
+
+
+% =====================[Sensor Dynamics]========================
+% TODO:
+Sens1    = 0;
+SensSat1 = Big;
+
+
+% =====================[Static Friction]========================
+% TODO:
+%StFric1 = 0; no friction q1 motor (according to simu blocks)
+
+
+
+% ==========================================
+% Transfer Functions
+% ==========================================
+% Compute transfer functions from above values and perform system analysis
+% You may prefer to put this section in a separate .m file
+
+% Amplifier Transfer Function
+tf_amp = tf(Amp0n, Amp0d);
+tf_elec = tf(Elec0n, Elec0d);
