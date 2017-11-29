b43f7126b2317cacf6f46e2da15558beae7c3b6e
Muchen He <mansurhe@outlook.com>
2017-11-28 21:47:37 -0800

    somethings wrong lol

--- printer/Control.m
+++ printer/Control.m
@@ -28,8 +28,11 @@ ol_q1 = zpk(minreal(oltf_q1));
 % Start by placing the zero at where we want the poles to cancel out
 zeroes_q0 = [1, 1.95, 96.77];
 
-% KGH after we applied the PID (two zeroes at our preferred lcoation + 1 pole at zero)
-kgh_q0 = tf(zeroes_q0, [1, 0]) * ol_q0;
+% PID provides us with one pole at zero and two zeroes we can use
+pid_zp_q0 = tf(zeroes_q0, [1, 0]);
+
+% The resulting KGH after adding two zeroes and one pole at zero
+kgh_q0 = pid_zp_q0 * ol_q0;
 
 % Find ultimate gain when system becomes unstable
 KU_q0 = margin(tf(kgh_q0));
@@ -40,8 +43,14 @@ Kp_q0       = Kd_q0 * zeroes_q0(2);
 Ki_q0       = Kd_q0 * zeroes_q0(3);
 startPID_q0 = [Kp_q0, Ki_q0, Kd_q0];
 
+% The PID transfer function should be
+% PID = Kd * (s^2 + (Kp/Kd)s + (Ki/Kd)s)
+%       --------------------------------
+%                        s
+pid_q0 = Kd_q0 * tf([1, (Kp_q0 / Kd_q0), (Ki_q0 / Kd_q0)], [1, 0]);
+
 % Open loop transfer function of q0 with PID
-kgh_q0 = Kd_q0 * kgh_q0;
+ol_pid_q0 = pid_q0 * ol_q0;
 
 % === Q1 ====
 % Same thing
@@ -57,12 +66,12 @@ startPID_q1 = [Kp_q1, Ki_q1, Kd_q1];
 kgh_q1 = Kd_q1 * kgh_q1;
 
 % =====================[Closed loop transfer functions]========================
-% Transfer functions without PID (Verified: correct)
+% Transfer functions without PID or PID = [1 0 0] (Verified: correct)
 cl_q0 = ol_q0 / (1 + ol_q0);
 cl_q1 = ol_q1 / (1 + ol_q1);
 
 % Transfer function with starting PID
-cl_start_pid_q0 = kgh_q0 / (1 + kgh_q0);
+cl_start_pid_q0 = ol_pid_q0 / (1 + ol_pid_q0);
 cl_start_pid_q1 = kgh_q1 / (1 + kgh_q1);
 
 % ================
@@ -73,6 +82,12 @@ cl_start_pid_q1 = kgh_q1 / (1 + kgh_q1);
 % No more than 3 significant figures per gain value.
 PID0 = [1 0 0];
 PID1 = [1 0 0];
+
+% PID0 = startPID_q0;
+
+% PID0 = [1.5024184383397 5.58181210407204 0.098054291994374];
+PID0 = [1.4500 14.0000 0.2800];
+PID1 = [0.238 0.131 0.0192];
 
 % Enter feedback sensor values here.
 % The feedback gain maps voltage (V) from [-5, 5] to angles (rad) [-pi, pi]
@@ -101,22 +116,20 @@ FB1 = FB0;
 % Final PID transfer functions
 % ==========================================
 % Transfer function with PID
-Kp0     = PID0(1);
-Ki0     = PID0(2);
-Kd0     = PID0(3);
-% PID0_tf = tf([Kd0 Kp0 Ki0],[1, 0]);
-% g_cl_q0 = PID0_tf * tf_amp0 * (g_q0 / (1 + g_q0 * h_q0)) / tf('s');
-% h_cl_q0 = Sens0;
-% cltf_q0 = g_cl_q0 / (1 + g_cl_q0 * h_cl_q0);
+PID0_tf = PID0(3) * tf([1, (PID0(1) / PID0(3)), (PID0(2) / PID0(3))], [1, 0]);
+g_cl_q0 = PID0_tf * ol_q0;
+cltf_q0 = g_cl_q0 / (1 + g_cl_q0);
+% step(cltf_q0);
+% hold on;
+% disp(stepinfo(cltf_q0));
 
 % Motor 1
-Kp1     = PID1(1);
-Ki1     = PID1(2);
-Kd1     = PID1(3);
-PID1_tf = tf([Kd1 Kp1 Ki1],[1, 0]);
-g_cl_q1 = PID1_tf * tf_amp1 * (g_q1 / (1 + g_q1 * h_q1)) / tf('s');
-h_cl_q1 = Sens1;
-cltf_q1 = g_cl_q1 / (1 + g_cl_q1 * h_cl_q1);
+PID1_tf = tf([1, (PID1(1) / PID1(3)), (PID1(2) / PID1(3))], [1, 0]);
+g_cl_q1 = PID1_tf * ol_q1;
+cltf_q1 = g_cl_q1 / (1 + g_cl_q1);
+step(cltf_q1);
+hold on;
+disp(stepinfo(cltf_q1));
 
 %some useful commands 
 %[k,poles] = rlocfind(sys)
