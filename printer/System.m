526fd4cbcaf5935eedc43e1a2e5d5dfb049251f6
Muchen He <mansurhe@outlook.com>
2017-11-28 17:36:54 -0800

    Migrated OL and CLTF to control.m

--- printer/System.m
+++ printer/System.m
@@ -45,8 +45,6 @@ joint_limit = JntLim * RadPerDeg;               % (rad)
 % ==========================
 % Choose Motors
 % ==========================
-
-
 % =====================[Motor Parameter Unit Conversion]========================
 motor_param_unit_convert = ...
 [
@@ -91,7 +89,8 @@ motor_param_unit_convert = ...
 % ==========================
 AMAX22_5W_SB;
 q0 = MotorParam .* motor_param_unit_convert;
-AMAX22_6W_SB;
+% AMAX22_6W_SB;
+AMAX12_p75W_SB;
 q1 = MotorParam .* motor_param_unit_convert;
 
 % =============================
@@ -346,68 +345,3 @@ tf_elec1 = tf(Elec1n, Elec1d);
 % Mechanical Dynamics Transfer Functions
 tf_mech0 = tf(Mech0n, Mech0d);
 tf_mech1 = tf(Mech1n, Mech1d);
-
-% ==========================================
-% Open loop transfer function of everything
-% ==========================================
-% The open loop transfer function maps voltage to radians/s
-
-% Motor 0
-g_q0 = tf_elec0 * TConst0 * tf_mech0;
-h_q0 = BackEMF0;
-oltf_q0 = tf_amp0 * (g_q0 / (1 + g_q0 * h_q0)) / tf('s') * Sens0; 
-
-% Simplified
-ol_q0 = zpk(minreal(oltf_q0));
-
-% Motor 1
-g_q1 = tf_elec1 * TConst1 * tf_mech1;
-h_q1 = BackEMF1;
-
-% Simplified
-ol_q1 = zpk(minreal(oltf_q1));
-oltf_q1 = tf_amp1 * (g_q1 / (1 + g_q1 * h_q1)) / tf('s') * Sens1;
-
-% ==========================================
-% Close loop transfer function of everything (might be useful in finding overall step/impulse response)
-% ==========================================
-
-% Motor 0
-% Kp0=PID0(1)
-% Ki0=PID0(2)
-% Kd0=PID0(3)
-% PID0_tf=tf([Kd0 Kp0 Ki0],[1, 0])
-% g_cl_q0 = PID0_tf* tf_amp0 * (g_q0 / (1 + g_q0 * h_q0)) / tf('s')
-% h_cl_q0 = Sens0
-% cltf_q0 = g_cl_q0 / (1 + g_cl_q0 * h_cl_q0)
-
-% % Motor 1
-% Kp1=PID1(1)
-% Ki1=PID1(2)
-% Kd1=PID1(3)
-% PID1_tf=tf([Kd1 Kp1 Ki1],[1, 0])
-% g_cl_q1 = PID1_tf* tf_amp1 * (g_q1 / (1 + g_q1 * h_q1)) / tf('s')
-% h_cl_q1 = Sens1
-% cltf_q1 = g_cl_q1 / (1 + g_cl_q1 * h_cl_q1)
-
-%some useful commands 
-%[k,poles] = rlocfind(sys)
-
-% =====================[PID testing]========================
-% Q0
-zeroes_q0 = [1, 0.2838, 14.11];
-ol_pid_q0 = ol_q0 * tf(zeroes_q0, [1, 0]);
-KU_q0 = margin(ol_pid_q0);
-Kd_q0 = KU_q0 / 2;
-Kp_q0 = Kd_q0 * zeroes_q0(2);
-Ki_q0 = Kd_q0 * zeroes_q0(3);
-PID_q0 = [Kd_q0, Kp_q0, Ki_q0];
-
-% Q1
-zeroes_q1 = [1, 49.17, 0];
-ol_pid_q1 = ol_q1 * tf(zeroes_q0, [1, 0]);
-KU_q1 = margin(ol_pid_q1);
-Kd_q1 = KU_q1 / 2;
-Kp_q1 = Kd_q1 * zeroes_q1(2);
-Ki_q1 = Kp_q1 * zeroes_q1(3);
-PID_q1 = [Kd_q1, Kp_q1, Ki_q1];
